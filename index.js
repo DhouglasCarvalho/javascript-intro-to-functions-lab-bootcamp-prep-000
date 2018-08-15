66ca0ac7c31f16f9de946fa0295c4ebe032bed85
Dhouglas Oaktree <dhouglas@gmail.com>
2018-08-15 17:05:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function sayHiToGrandma(string) {
-var lowercase = "I can't hear you!"
-
- mixedCased.toLowerCase() === lowercase
-
-  console.log(sayHiToGrandma)
+function logShout(String) {
+var uppercase.toUpperCase() === uppercase //true
+  console.log("logShout")
 }
