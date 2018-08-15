8aa1e34c0063dd345ea27c86cae6649f29151341
Dhouglas Oaktree <dhouglas@gmail.com>
2018-08-15 17:46:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function sayHiToGrandma(string) {
   mixedCase.toLowerCase() === mixedCase
   mixedCase.toUpperCase() === mixedCase
 
-console.log("I can't hear you!")
+return string.toMixedCase()
 
 }
