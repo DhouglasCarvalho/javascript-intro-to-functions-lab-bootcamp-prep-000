f2d9467eb8400d126e5f0f31a63a6bd21368ac04
Dhouglas Oaktree <dhouglas@gmail.com>
2018-08-15 17:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function sayHiToGrandma(string) {
   mixedCase.toLowerCase() === mixedCase
   mixedCase.toUpperCase() === mixedCase
 
-return sayHiToGrandma()
+return sayHiToGrandma.toUpperCase()
 
 }
